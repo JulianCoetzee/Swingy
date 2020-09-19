4ab9bf969f4b0777e06a372e3e2cd8904ffccf85
JulianCoetzee <jcoetzee@student.wethinkcode.co.za>
2020-09-19 11:30:10 +0200

    new test. display on alternate setup

--- src/main/java/jules/rpg/App.java
+++ src/main/java/jules/rpg/App.java
@@ -1,5 +1,7 @@
 package jules.rpg;
 
+import jules.rpg.gui.GuiBuild;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        GuiBuild GUI = new GuiBuild();
     }
 }
